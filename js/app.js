884416f480074ba8f406d0dbd07cf4e0a914b78c
mouss2388 <mouss2388@gmail.com>
2021-01-25 21:45:00 +0100

    Feat
    Auto play trailer

--- js/app.js
+++ js/app.js
@@ -1,17 +1,40 @@
 const listItems = document.getElementsByClassName("item-list");
+const btn_video = document.getElementsByClassName("btn");
+const video = document.querySelector("iframe");
 
+function getFullScreenElement() {
+  return (
+    document.fullscreenElement ||
+    document.webkitFullscreenElement ||
+    document.mozFullscreenElement ||
+    document.msFullscreenElement
+  );
+}
+
+function toggleFullscreen() {
+  if (getFullScreenElement()) {
+    video.exitFullscreen();
+  } else {
+    video.requestFullscreen().catch(console.log);
+  }
+}
+
+btn_video.item(0).addEventListener("click", function () {
+  video.classList.toggle("hidden");
+  video.requestFullscreen().catch((e) => {});
+});
 
 function gestionNav() {
-    for (let index_target = 0; index_target < listItems.length; index_target++) {
-        listItems.item(index_target).addEventListener("click", (e) => {
-            `${e.currentTarget.classList.add("active")}`;
-            for (idx = 0; idx < listItems.length; idx++) {
-                if (idx !== index_target) {
-                    listItems.item(idx).classList.remove("active");
-                }
-            }
-        });
-    }
+  for (let index_target = 0; index_target < listItems.length; index_target++) {
+    listItems.item(index_target).addEventListener("click", (e) => {
+      `${e.currentTarget.classList.add("active")}`;
+      for (idx = 0; idx < listItems.length; idx++) {
+        if (idx !== index_target) {
+          listItems.item(idx).classList.remove("active");
+        }
+      }
+    });
+  }
 }
 
 gestionNav();
